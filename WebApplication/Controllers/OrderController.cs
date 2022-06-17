783286b70e2bd7a159ecd7afa233b0374bf58534
sudowcam <sudowcam@gmail.com>
2022-06-17 21:44:58 +0800

    Order details page

--- WebApplication/Controllers/OrderController.cs
+++ WebApplication/Controllers/OrderController.cs
@@ -14,6 +14,86 @@ namespace Internal.Controllers
     {
         private NorthwindEntities db = new NorthwindEntities();
 
+
+        private IReadOnlyCollection<OrderDetailsModel> MapOrderDetails()
+        {
+            return (from m in db.OrderDetails.AsEnumerable() select new OrderDetailsModel()
+            {
+                CategoryID = m.Products.CategoryID.Value,
+                CategoryName = m.Products.Categories.CategoryName,
+                OrderTotalCost = m.Quantity * m.UnitPrice * (decimal)(1 - m.Discount),
+                OrderID = m.OrderID,
+                OrderQuantity = m.Quantity,
+                ProductID = m.ProductID,
+                ProductName = m.Products.ProductName,
+                ShipperID = m.Orders.Shippers.ShipperID,
+                ShipperCompanyName = m.Orders.Shippers.CompanyName,
+            }).ToList();
+        }
+
+        public ActionResult Details(string Mode, int? OrderID)
+        {
+            if (!(Mode == "View" || Mode == "Edit"))
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DropDownList_CustomerName = db.Customers.OrderBy(x => x.ContactName).Select(x => new SelectListItem
+            { 
+                Text = x.ContactName,
+                Value = x.CustomerID.ToString()
+            });
+            ViewBag.DropDownList_EmployeeName = db.Employees.OrderBy(x => x.FirstName).ThenBy(x => x.LastName).Select(x => new SelectListItem
+            {
+                Text = x.FirstName + " " + x.LastName,
+                Value = x.EmployeeID.ToString()
+            });
+            ViewBag.DropDownList_CategoryName = db.Categories.OrderBy(x => x.CategoryName).Select(x => new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value = x.CategoryID.ToString()
+            });
+
+            var orderDetailModel = (from m in MapOrderDetails() where m.OrderID == OrderID select m);
+            var model = new OrderDetailPageModel();
+
+            if (OrderID == null)
+            {
+                if (Mode == "View")
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                if (orderDetailModel != null)
+                {
+                    var mOrder = db.Orders.Find(OrderID);
+                    model = new OrderDetailPageModel
+                    {
+                        OrderDetailList = orderDetailModel,
+                        CustomerAddress = mOrder.Customers.Address + ", " +
+                                          mOrder.Customers.PostalCode + ", " +
+                                          mOrder.Customers.City + ", " +
+                                          mOrder.Customers.Country,
+                        CustomerContactNumber = mOrder.Customers.Phone,
+                        CustomerID = mOrder.CustomerID,
+                        CustomerName = mOrder.Customers.ContactName,
+                        EmployeeID = mOrder.EmployeeID.Value,
+                        EmployeeName = mOrder.Employees.FirstName + " " +
+                                       mOrder.Employees.LastName,
+                        OrderDate = mOrder.OrderDate.Value,
+                        OrderGrandTotal = orderDetailModel.Sum(x=>x.OrderTotalCost),
+                    };
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+
         public ActionResult Index(
         string Page,
         string Search,
--- WebApplication/Models/Order.cs
+++ WebApplication/Models/Order.cs
@@ -17,4 +17,57 @@ namespace Internal.Models
         public decimal TotalCost { get; set; }
     }
 
+    public class OrderDetailsModel
+    { 
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Please select Product Category.")]
+        public string CategoryName { get; set; }
+
+        public decimal OrderTotalCost { get; set; }
+
+        //public float OrderDiscount { get; set; }
+
+        public int OrderID { get; set; }
+
+        public int OrderQuantity { get; set; }
+
+        //public decimal OrderUnitPrice { get; set; }
+
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please select Product Name.")]
+        public string ProductName { get; set; }
+
+        //public short ProductUnitsInStock { get; set; }
+
+        public int ShipperID { get; set; }
+
+        public string ShipperCompanyName { get; set; }
+    }
+
+    public class OrderDetailPageModel
+    {
+        public IEnumerable<OrderDetailsModel> OrderDetailList { get; set; }
+
+        public string CustomerAddress { get; set; }
+
+        public string CustomerContactNumber { get; set; }
+
+        public string CustomerID { get; set; }
+
+        [Required(ErrorMessage = "Please select Customer Name.")]
+        public string CustomerName { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "Please select Employee Name.")]
+        public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "Please select Order Date.")]
+        public DateTime OrderDate { get; set; }
+
+        public decimal OrderGrandTotal { get; set; }
+    }
+
 }
